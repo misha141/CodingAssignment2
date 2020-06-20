7c6078032807defbfce615ba91082993521984ab
Misha Kumari <mishasingh141@gmail.com>
2020-06-20 18:38:54 +0530

    Adding multiple documents to firebase

--- app/src/main/java/com/example/codingassignment2/MainActivity.java
+++ app/src/main/java/com/example/codingassignment2/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.codingassignment2;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button createEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createEvent =(Button)findViewById(R.id.create_event);
+        createEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,DefineSlotsActivitiy.class);
+                startActivity(intent);
+            }
+        });
     }
 }
